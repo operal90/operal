519a7d8f0855d832a7e729b2d11029996582da2a
Jafar-design <jafarjamiu@gmail.com>
2023-04-13 12:40:50 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}",
+
+  '*.{html,js}'], //like this
+
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
+        }
+      },
+
+      fontFamily: {
+        body: ['Nunito']
+      }
+    },
+  },
+  plugins: [],
+}
